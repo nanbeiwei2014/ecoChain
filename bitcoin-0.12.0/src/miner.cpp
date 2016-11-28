563d4308a986e87c10c0855d8ccaa1f3a6c3499c
sunyl <sunyl@644ef79d-13d7-4a29-8f56-b9c6571746a5>
2016-11-28 07:38:19 +0000

    Add print newblockcreatetime log

--- bitcoin-0.12.0/src/miner.cpp
+++ bitcoin-0.12.0/src/miner.cpp
@@ -603,13 +603,13 @@ void static BitcoinMiner(const CChainParams& chainparams)
             {
             	if ((*iter)->m_bNetState) nConnectCount++;
             }
-//            vector<CNode*>::iterator iter = std::min_element(g_vAllNodes.begin(), g_vAllNodes.end(), SortVNodesBy);
-            vector<CNode*>::iterator iter=g_vAllNodes.begin();
-            if ((0.05*DEFAULT_GENERATE_PERIOD)>fabs(g_vAllNodes[0]->m_creBlockTime - g_vAllNodes[1]->m_creBlockTime ))
-            {
-            	if (g_vAllNodes[0]->addr.ToStringIP() > g_vAllNodes[1]->addr.ToStringIP() )
-            	iter++;
-            }
+            vector<CNode*>::iterator iter = std::min_element(g_vAllNodes.begin(), g_vAllNodes.end(), SortVNodesBy);
+//            vector<CNode*>::iterator iter=g_vAllNodes.begin();
+//            if ((0.05*DEFAULT_GENERATE_PERIOD)>fabs(g_vAllNodes[0]->m_creBlockTime - g_vAllNodes[1]->m_creBlockTime ))
+//            {
+//            	if (g_vAllNodes[0]->addr.ToStringIP() > g_vAllNodes[1]->addr.ToStringIP() )
+//            	iter++;
+//            }
 
              std::string strIp=(*iter)->addr.ToStringIP();
             if((VALID_BLOCK_NODES<nConnectCount)&&(std::string::npos != strIp.find("127.0.0.1")))
