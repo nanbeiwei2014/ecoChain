1c54df6af8f1c9ff63a041d7dac854c1ce9d03ca
sunyl <sunyl@644ef79d-13d7-4a29-8f56-b9c6571746a5>
2016-11-30 03:11:43 +0000

    cre branches

---
+++ branches/bitcoin-0.12.0/src/primitives/block.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "primitives/block.h"
+
+#include "hash.h"
+#include "tinyformat.h"
+#include "utilstrencodings.h"
+#include "crypto/common.h"
+
+uint256 CBlockHeader::GetHash() const
+{
+    return SerializeHash(*this);
+}
+
+std::string CBlock::ToString() const
+{
+    std::stringstream s;
+    s << strprintf("CBlock(hash=%s, ver=%d, hashPrevBlock=%s, hashMerkleRoot=%s, nTime=%u, "\
+    		"nBits=%08x,"
+    		" nNonce=%u, q"
+    		"vtx=%u)\n",
+        GetHash().ToString(),
+        nVersion,
+        hashPrevBlock.ToString(),
+        hashMerkleRoot.ToString(),
+        nTime,
+		nBits,
+		nNonce,
+        qvtx.size());
+    for (unsigned int i = 0; i < qvtx.size(); i++)
+    {
+        s << "  " << qvtx[i].to_string() << "\n";
+    }
+    return s.str();
+}
