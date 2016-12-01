54853df159e5c18029712b350874dec7a13d44b0
mengqg <gaodedashu@tom.com>
2016-12-01 01:07:41 +0000

    mengqg

--- bitcoin-0.12.0/src/miner.cpp
+++ bitcoin-0.12.0/src/miner.cpp
@@ -618,7 +618,9 @@ void static BitcoinMiner(const CChainParams& chainparams)
             unsigned int nTransactionsUpdatedLast = qmempool.get_data_updated();
             CBlockIndex* pindexPrev = chainActive.Tip();
 
-
+            while(!pindexPrev){
+            	pindexPrev = chainActive.Tip();
+            }
 
 //            unsigned int nConnectCount = 0;
 //
