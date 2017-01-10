e3ad78ebeb8fa68ca6d0e6dec1cd520b7ed5eb92
sundk <sundk@644ef79d-13d7-4a29-8f56-b9c6571746a5>
2017-01-10 07:56:49 +0000

    add whather letter upper

--- bitcoin-0.12.0/src/rpcrawtransaction.cpp
+++ bitcoin-0.12.0/src/rpcrawtransaction.cpp
@@ -45,7 +45,10 @@ bool StrToLower( string &str )
 {
 	for( unsigned int i = 0; i < str.length(); i++ )
 	{
-		str[i] = str[i] + 32;
+        if ( str[i] >='A' && str[i] <= 'Z' )
+        {
+            str[i] = str[i] + 32;
+        }
 	}
 	return true;
 }
